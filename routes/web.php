fed088e55d45ba1f3d14538549292ad112f051cb
dev-alamin04 <aak75049@gmail.com>
2026-07-08 09:01:16 +0600

    change composer file

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 // Payment Routes
 
 use App\Http\Controllers\Web\Backend\Quest\QuestController;
-use App\Http\Controllers\Web\Payment\StripePaymentController;
 
 use Illuminate\Support\Facades\Route;
-
-Route::prefix('payments')->name('payments.')->group(function () {
-    Route::get('/success', [StripePaymentController::class, 'success'])->name('success');
-    Route::get('/cancel', [StripePaymentController::class, 'cancel'])->name('cancel');
-});
 
 require __DIR__ . '/auth.php';
