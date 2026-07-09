b5ef305829179aa93f6752e4541921f1bd3cef6f
dev-alamin04 <aak75049@gmail.com>
2026-07-09 15:42:37 +0600

    3 pro key

--- app/Mail/SecretKeyMail.php
+++ app/Mail/SecretKeyMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SecretKeyMail extends Mailable implements ShouldQueue
+class SecretKeyMail extends Mailable
 {
     use Queueable, SerializesModels;
 
